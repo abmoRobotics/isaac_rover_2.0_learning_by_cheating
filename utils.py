8c769347d7c891258833e1c35fc0975f15f24fa1
Lionel Burg <lionel@blueatlasrobotics.com>
2022-11-17 14:29:25 +0100

    fix sort_data

--- utils.py
+++ utils.py
@@ -4,14 +4,20 @@ from tqdm import tqdm
 
 
 def sort_data(path):
+    os.remove(path + "data.pt")
     path = path
     files = os.listdir(path)
-    if not "data.pt" in files:
-        data_path = os.path.join(path, files[0])
-        data = torch.load(data_path)
-        for i in (pbar := tqdm(range(1, len(files)))):
-            pbar.set_description("Processing data")
-            data_path = os.path.join(path, files[i])
-            data_temp = torch.load(data_path)
-            data = torch.cat((data, data_temp), dim=0)
-        torch.save(data, path + "data.pt")
+    data_path = os.path.join(path, files[0])
+    data_full = torch.load(data_path)
+    info = data_full["info"]
+    data = data_full["data"]
+    for i in (pbar := tqdm(range(1, len(files)))):
+        #pbar.set_description("Processing data")
+        data_path = os.path.join(path, files[i])
+        data_temp = torch.load(data_path)["data"]
+        print(data_temp)
+        data = torch.cat((data, data_temp), dim=0)
+
+    file = {"info": info,
+            "data": data}
+    torch.save(file, path + "data.pt")
